ee23d4c841fbfdea7d5c45f8ac835eb028b50167
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2024-05-30 15:20:41 +0700

    Add files via upload

---
+++ DatabaseRequests.cs
@@ -0,0 +1,143 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using Npgsql;
+
+namespace GarageConsoleApp;
+
+
+public static class DatabaseRequests
+{
+    static string formattedDate = "";
+    public static void GetAllUserQuests(int numberID)
+    {
+        var querySql = $"SELECT datequest, quest, completed FROM sidequests WHERE ID_user = {numberID}";
+
+
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+
+
+        using var reader = cmd.ExecuteReader();
+
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Дата: {reader[0]} \nЗадание: {reader[1]} \nЗавершено: {reader[2]}");
+        }
+    }
+
+    public static void Registr(string login, string password)
+    {
+        var querySql = $"insert into userside (login, password) values ('{login}','{password}')";
+
+
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+
+
+        using var reader = cmd.ExecuteReader();
+
+    }
+
+
+    public static int Entrance(string login, string password)
+    {
+        var querySql = $"select id from userside where login = '{login}' and password = '{password}'";
+
+
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+
+
+        using var reader = cmd.ExecuteReader();
+
+        if (reader.Read())
+        {
+            int userID = reader.GetInt32(0);
+            Console.WriteLine("Вход выполнен успешно");
+            return userID;
+        }
+        else        {
+            Console.WriteLine("Неверный логин или пароль");
+            return -1;
+        }
+    }
+
+
+
+
+   
+
+      public static void AddNewQuest(string quest, int userID)
+    {
+        Console.WriteLine("Введите дату выполнения задания в формате (гггг-мм-дд чч:мм):");
+        DateTime dateOfCompletion;
+        string formattedDate;
+        try        {
+            dateOfCompletion = DateTime.Parse(Console.ReadLine());
+            formattedDate = dateOfCompletion.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        catch        {
+            Console.WriteLine("Неверный формат даты");
+            return;
+        }
+
+        var querySql =
+            $"INSERT INTO sidequests(datequest, quest, completed, id_user) VALUES ('{formattedDate}', '{quest}', false, {userID})";
+        using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+        cmd.ExecuteNonQuery();
+    }
+      
+      public static void DeleteQuest(int userID)
+      {
+          var querySql = $"DELETE FROM sidequests WHERE id_user = {userID}";
+          using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+          cmd.ExecuteNonQuery();
+      }
+      
+      
+      public static void TodayQuest(int userID)
+      {
+          formattedDate = DateTime.Today.ToString("yyyy-MM-dd HH:mm:ss");
+            
+          var querySql = $"SELECT * FROM sidequests where datequest = '{formattedDate}' and id_user = {userID}";
+          using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+          using var reader = cmd.ExecuteReader();
+          while (reader.Read())
+          {
+              Console.WriteLine(
+                  $"Название: {reader[1]} \nЗадача: {reader[2]} \nДата выполнения: {reader[3]}");
+          }
+      }
+      public static void TommorowQuest(int userID)
+      {
+          formattedDate = DateTime.Today.AddDays(1).ToString("yyyy-MM-dd HH:mm:ss");
+        
+          var querySql = $"SELECT * FROM sidequests where datequest = '{formattedDate}' and id_user = {userID}";
+          using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+          using var reader = cmd.ExecuteReader();
+          while (reader.Read())
+          {
+              Console.WriteLine(
+                  $"Название: {reader[1]} \nЗадача: {reader[2]} \nДата выполнения: {reader[3]}");
+          }
+      }
+
+      public static void WeekQuest(int userID)
+      {
+          int weekConvert = (int)DateTime.Today.DayOfWeek;
+          if (weekConvert == 0)
+          {
+              weekConvert = 7;
+          }
+
+          formattedDate = DateTime.Today.ToString("yyyy-MM-dd HH:mm:ss");
+          string formattedDateWeekConvert = DateTime.Today.AddDays(7 - weekConvert).ToString("yyyy-MM-dd HH:mm:ss");
+        
+          var querySql =
+              $"SELECT * FROM sidequests where (datequest >= '{formattedDate}' AND datequest <= '{formattedDateWeekConvert}') and id_user = {userID}";
+          using var cmd = new NpgsqlCommand(querySql, DatabaseService.GetSqlConnection());
+          using var reader = cmd.ExecuteReader();
+          while (reader.Read())
+          {
+              Console.WriteLine(
+                  $"Название: {reader[1]} \nЗадача: {reader[2]} \nДата выполнения: {reader[3]}");
+          }
+      }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿namespace GarageConsoleApp;
+
+/// <summary>
+/// Класс Program
+/// здесь описываем логику приложения
+/// вызываем нужные методы, пишем обработчики и т.д.
+/// </summary>
+public class Program
+{
+ public static void Main(string[] args)
+ {
+  int oneplace = 0;
+  int userID = 0;
+  string? bang1 = Console.ReadLine();
+
+  while (bang1 != "Q")
+  {
+   switch (bang1)
+   {
+    case "1":
+     userID = DatabaseRequests.Entrance(Console.ReadLine(), Console.ReadLine());
+     Console.WriteLine(userID);
+     break;
+    case "2":
+     DatabaseRequests.Registr(Console.ReadLine(), Console.ReadLine());
+     break;
+   }
+
+   string bang2 = Console.ReadLine();
+
+
+   while (bang2 != "Q")
+   {
+    switch (bang2)
+    {
+
+
+     case "1":
+        DatabaseRequests.GetAllUserQuests(userID);
+      break;
+
+     case "2":
+      Console.WriteLine("Введите описание задания:");
+      string questDescription = Console.ReadLine();
+
+      DatabaseRequests.AddNewQuest(questDescription, userID);
+      break;
+
+      case "3":
+       DatabaseRequests.TodayQuest(userID);
+       break;
+      
+      case "4":
+       DatabaseRequests.TommorowQuest(userID);
+       break;
+      
+      case "5":
+       DatabaseRequests.WeekQuest(userID);
+       break;
+      
+      case "6":
+       DatabaseRequests.DeleteQuest(userID);
+       break;
+
+
+     default:
+      
+      oneplace++;
+      if (oneplace == 2)
+      {
+       Console.WriteLine($"Подумай пожалуйста");
+      }
+      else if (oneplace == 10)
+      {
+       Console.WriteLine($"Чувак лол");
+      }
+      else if(oneplace == 11)
+      {
+       Console.WriteLine($"\nВстань мид! \nТы к**ченый? \nСтань и стой со мной, бл**ь. Финшман, ты х**ришь по 20 мм'ов в день, на**я? \nДля чего? \nОбъяснишь мне? \nТы е**ный крип, крипочек");
+      }
+      else
+      {
+       Console.WriteLine($"Извините, технические шоколадки(");
+      }
+      break;
+
+
+
+
+    }
+
+    bang2 = Console.ReadLine();
+   }
+
+   bang1 = Console.ReadLine();
+  }
+ }
+}
+  
+
